c730e4ac3dbf81b829328096b7f0b5b22738ef21
Clayton Mork <mork.clayton3@gmail.com>
2023-06-21 12:36:28 +0900

    Moved evaluation related code into a package folder named detr for easy use in other projects.

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import codecs
+import os.path
+
+packages = find_packages(
+        where='.',
+        include=[
+            'detr*',
+            # 'cli*'
+        ]
+)
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+setup(
+    name='detr',
+    version=get_version("detr/__init__.py"),
+    description='ported from facebookresearch/detr',
+    author='Clayton Mork',
+    author_email='mork.clayton3@gmail.com',
+    license='MIT License',
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # 'argcomplete>=1.12.3',
+        # 'pylint>=2.12.2',
+        # 'numpy>=1.21.5'
+    ],
+    python_requires='>=3.9',
+    # entry_points={
+    #     "console_scripts": [
+    #         "detr=cli.example:main"
+    #     ]
+    # }
+)
